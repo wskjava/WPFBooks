02242c34fe112e2e5d3330141db1936c435a91ef
284630664@qq.com <284630664@qq.com>
2018-01-05 23:42:26 +0800

    Initial commit

---
+++ WPFBooksDemo/WPFBooksDemo/InheritTheApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFBooksDemo
+{
+    class InheritTheApp: Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            InheritTheApp app = new InheritTheApp();
+            app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Window win = new Window();
+            win.Title = "Inherit the App";
+            win.Show();
+        }
+    }
+}
---
+++ WPFBooksDemo/WPFBooksDemo/SayHello.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFBooksDemo
+{
+    class SayHello
+    {
+        //[STAThread]
+        //public static void Main()
+        //{
+        //    Window win = new Window();
+        //    win.Title = "Say Hello";
+        //    win.MouseDown += Win_MouseDown;
+        //   // win.Show();
+
+        //    Application app = new Application();
+        //    //app.Run();
+
+        //    app.Run(win);
+        //}
+
+        private static void Win_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Window win = sender as Window;
+            string strMessage =
+                string.Format("Window clickd with {0} button at point ({1}) )",
+                e.ChangedButton, e.GetPosition(win));
+            //MessageBox.Show(strMessage, win.Title);
+
+            MessageBox.Show(strMessage, Application.Current.MainWindow.Title);
+        }
+    }
+}
